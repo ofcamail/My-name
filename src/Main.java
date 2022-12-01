9d1262ba2960a4d9556170d92364e42b14493ca2
ofcmail <ofcmailkom@gmail.com>
2022-12-01 17:43:41 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, меня зовут Лена!");
+        System.out.println("Привет, меня зовут Лена");
     }
 }
